856a528badf6223cd9e56fc6e6ce57b6c520cb09
jinhengpang <pangjinheng58@gmail.com>
2025-12-13 22:29:26 +1100

    Add carousel arrows

--- front-end/src/_components/svgs/index.ts
+++ front-end/src/_components/svgs/index.ts
@@ -2,3 +2,5 @@ export { default as SvgGoogle } from "./GoogleLogo";
 export { default as SvgMicrosoft } from "./MicrosoftLogo";
 export { default as SvgMjc } from "./MjcLogo";
 export { default as SvgMjcModern } from "./MjcLogoModern";
+export { default as SvgCarouselRightArrow } from "./CarouselRightArrow";
+export { default as SvgCarouselLeftArrow } from "./CarouselLeftArrow";
